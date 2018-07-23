3ebf0ae630d5d6e141e19b7f9ab9ac6510d932dc
Yuchen Huang <searchingundermoon@gmail.com>
2018-07-22 21:33:27 -0700

    7.22.2018

---
+++ Linear Structure/String/824. Goat Latin/solution.java
@@ -0,0 +1,31 @@
+
+
+class Solution {
+    public String toGoatLatin(String S) {
+        if(S.length() == 0) return S;
+        String[] arr = S.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < arr.length; i ++)
+        {
+            if(checkVowel(arr[i]))
+            {
+                sb.append(arr[i]);
+            }
+            else
+            {
+                sb.append(arr[i].substring(1, arr[i].length()));
+                sb.append(arr[i].charAt(0));
+            }
+            sb.append("ma");
+            for(int j = 0; j <= i; j++) sb.append('a');
+            if(i != arr.length - 1) sb.append(' ');
+        }
+        return sb.toString();
+    }
+    public boolean checkVowel(String s)
+    {
+        char[] vowels = new char[]{'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        for(char c : vowels) if(s.charAt(0) == c) return true;
+        return false;
+    }
+}
