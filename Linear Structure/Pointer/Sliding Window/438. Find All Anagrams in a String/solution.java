3ce189f3d226068af28b2f5073e8405fbeb60991
Yuchen Huang <searchingundermoon@gmail.com>
2018-08-20 10:22:45 +0800

    8.20.2018

---
+++ Linear Structure/Pointer/Sliding Window/438. Find All Anagrams in a String/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> ret = new LinkedList();
+        if(s.length() < p.length()) return ret;
+        char[] arr = s.toCharArray();
+        int[] map = new int[26];
+        for(int i = 0; i < p.length(); i ++) map[p.charAt(i) - 'a'] ++;
+        for(int i = 0; i < p.length(); i ++) map[s.charAt(i) - 'a'] --;
+        if(checkMap(map)) ret.add(0);
+        for(int i = p.length(); i < s.length(); i++)
+        {
+            map[arr[i-p.length()] - 'a'] ++;
+            map[arr[i] - 'a'] --;
+            if(checkMap(map)) ret.add(i-p.length() + 1);
+        }
+        return ret;
+    }
+    public boolean checkMap(int[] map)
+    {
+        for(int i = 0; i < map.length; i ++)
+            if(map[i] > 0) return false;
+        return true;
+    }
+}
+
