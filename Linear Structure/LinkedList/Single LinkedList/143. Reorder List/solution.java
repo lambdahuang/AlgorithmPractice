517b8b11e9e903c918f041e2a8aae4124b68f94e
Yuchen Huang <searchingundermoon@gmail.com>
2018-08-21 16:11:13 +0800

    8.21.2018

---
+++ Linear Structure/LinkedList/Single LinkedList/143. Reorder List/solution.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if(head == null || head.next == null) return;
+        Stack<ListNode> st = new Stack();
+        ListNode tmp = head;
+        while(tmp != null)
+        {
+            st.push(tmp);
+            tmp = tmp.next;
+        }
+        tmp = head;
+        while(true)
+        {
+            ListNode tail = st.pop();
+            ListNode orig = tmp.next;
+            tmp.next = tail;
+            if(tail == orig)
+            {
+                tail.next = null;
+                break;
+            }
+            else if(orig.next == tail)
+            {
+                tail.next = orig;
+                orig.next = null;
+                break;
+            }
+            else
+            {
+                tail.next = orig;
+                tmp = orig;
+            }
+        }
+    }
+}
