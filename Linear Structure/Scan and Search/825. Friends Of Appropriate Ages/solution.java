93a58b258573201452d70a67a4d3537145e19843
Yuchen Huang <searchingundermoon@gmail.com>
2018-10-02 17:37:57 -0400

    10.2.2018

---
+++ Linear Structure/Scan and Search/825. Friends Of Appropriate Ages/solution.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int numFriendRequests(int[] ages) {
+        if(ages.length <=1) return 0;
+        Arrays.sort(ages);
+        int[] map = new int[ages.length];
+        TreeMap<Integer, Integer> tm_lower_bound = new TreeMap();
+        TreeMap<Integer, Integer> tm_higher_bound = new TreeMap();
+        for(int i = 0; i < ages.length;i ++)
+            if(i == 0 || ages[i] != ages[i-1])
+                tm_lower_bound.put(ages[i], i);
+        for(int i = ages.length-1; i >= 0;i --)
+            if(i == ages.length-1 || ages[i] != ages[i+1])
+                tm_higher_bound.put(ages[i], i);
+        for(int i = 0; i < ages.length; i ++)
+        {
+            if(i == 0 || ages[i]!=ages[i-1])
+            {
+                int lowerBound = (int)(ages[i] *0.5+7);
+                int higherBound = ages[i];
+                Integer tmp;
+                tmp = tm_lower_bound.higherKey(lowerBound);
+                int indexLowerBound = tm_lower_bound.get(tmp);
+                int indexHigherBound = tm_higher_bound.get(ages[i]);
+                map[i] = Math.max(indexHigherBound - indexLowerBound, 0);
+                //System.out.println(i + " " + map[i]);
+            }
+            else
+            {
+                map[i] = map[i-1];
+            }
+        }
+        int count = 0;
+        for(int x: map) count+=x;
+        return count;
+    }
+}
+
