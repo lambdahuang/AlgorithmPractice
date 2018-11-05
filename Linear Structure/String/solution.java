5c985e4a7ccca772d03d0c8bf65a72d2bc571848
Yuchen Huang <searchingundermoon@gmail.com>
2018-11-05 16:40:45 -0500

    11.5.2018

---
+++ Linear Structure/String/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows == 1) return s;
+        StringBuilder[] lst = new StringBuilder[numRows];
+        for(int i = 0; i < lst.length; i ++)
+            lst[i] = new StringBuilder();
+        int peroid = numRows * 2 - 2;
+        for(int i = 0; i < s.length(); i ++)
+        {
+            char c = s.charAt(i);
+            int pos = i % peroid;
+            if(pos < numRows)
+                lst[pos].append(c);
+            else
+                lst[numRows - 1 - (pos - numRows + 1)].append(c);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(StringBuilder x: lst)
+            sb.append(x.toString());
+        
+        return sb.toString();
+    }
+}
